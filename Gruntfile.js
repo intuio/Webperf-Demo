48295c30be7ae2aa92d53f08880c48c2a4a67173
Bjoern Ganslandt <bjoern.ganslandt@intuio.at>
2015-06-26 09:44:53 +0200

    add more performant rotate variant

--- Gruntfile.js
+++ Gruntfile.js
@@ -152,7 +152,8 @@ module.exports = function (grunt) {
     // Compiles Sass to CSS and generates necessary files if requested
     sass: {
       options: {
-        loadPath: 'bower_components'
+        loadPath: 'bower_components',
+        compass: true
       },
       dist: {
         files: [{
