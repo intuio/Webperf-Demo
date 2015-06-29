162b500132ed8602c3ea61e272203c2459c51775
Bjoern Ganslandt <bjoern.ganslandt@intuio.at>
2015-06-29 17:31:21 +0200

    circles don't smile

--- app/scripts/main-2-no-blocking.js
+++ app/scripts/main-2-no-blocking.js
@@ -1,20 +1,20 @@
 'use strict';
 
-var smileys      = document.getElementsByClassName('smiley'),
+var circles      = document.getElementsByClassName('circle'),
     time         = 0,
     elementWidth = 100;
 
-function moveSmiley(i, elementWidth) {
-  var left = Math.sin(time + i * (6.3/smileys.length)) * 2*elementWidth + 300 + 'px';
-  var top = Math.cos(time + i * (6.3/smileys.length)) * 2*elementWidth + 300 + 'px';
+function moveCircle(i, elementWidth) {
+  var left = Math.sin(time + i * (6.3/circles.length)) * 2*elementWidth + 300 + 'px';
+  var top = Math.cos(time + i * (6.3/circles.length)) * 2*elementWidth + 300 + 'px';
   return 'left:' + left + '; top:' + top + ';';
 }
 
 function update() {
   time += 0.01;
 
-  for(var i=0; i < smileys.length; i++) {
-    smileys[i].style.cssText = moveSmiley(i, elementWidth);
+  for(var i=0; i < circles.length; i++) {
+    circles[i].style.cssText = moveCircle(i, elementWidth);
   }
 
   setTimeout(update, 16);
--- app/scripts/main-4-transforms.js
+++ app/scripts/main-4-transforms.js
@@ -1,20 +1,20 @@
 'use strict';
 
-var smileys      = document.getElementsByClassName('smiley'),
+var circles      = document.getElementsByClassName('circle'),
     time         = 0,
     elementWidth = 100;
 
-function moveSmiley(i, elementWidth) {
-  var left = Math.sin(time + i * (6.3/smileys.length)) * 2*elementWidth + 200 + 'px';
-  var top = Math.cos(time + i * (6.3/smileys.length)) * 2*elementWidth + 200 + 'px';
+function moveCircle(i, elementWidth) {
+  var left = Math.sin(time + i * (6.3/circles.length)) * 2*elementWidth + 200 + 'px';
+  var top = Math.cos(time + i * (6.3/circles.length)) * 2*elementWidth + 200 + 'px';
   return 'translate(' + left + ', ' + top + ')';
 }
 
 function update() {
   time += 0.01;
 
-  for(var i=0; i < smileys.length; i++) {
-    smileys[i].style.transform = moveSmiley(i, elementWidth);
+  for(var i=0; i < circles.length; i++) {
+    circles[i].style.transform = moveCircle(i, elementWidth);
   }
 
   requestAnimationFrame(update);
